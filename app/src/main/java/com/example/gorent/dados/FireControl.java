f8fa395ff24e6ba69e3f3bcd8d3997569a88e307
Luiz Magno <luiz.mg@hotmail.com>
2017-01-17 20:08:53 -0300

    Update FireControl.java
    atualização da segunda iteração

--- app/src/main/java/com/example/gorent/dados/FireControl.java
+++ app/src/main/java/com/example/gorent/dados/FireControl.java
@@ -3,6 +3,7 @@ package com.example.gorent.dados;
 import android.util.Log;
 
 import com.example.gorent.entidade.Car;
+import com.example.gorent.entidade.Client;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,31 +25,28 @@ public class FireControl {
 
     private static final String TAG = "FireControl";
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("car");
+    DatabaseReference myCarRef = database.getReference("car");
+    DatabaseReference myClientRef = database.getReference("client");
 
     public void writeNewCar(Car car){
-        String key = myRef.push().getKey();
+        String key = myCarRef.push().getKey();
         Map<String, Object> carroValues = car.toMap();
-
         Map<String, Object> childUpdates = new HashMap<>();
         childUpdates.put(key, carroValues);
-        myRef.updateChildren(childUpdates);
+        myCarRef.updateChildren(childUpdates);
     }
 
-    public ArrayList<Car> retrive_all_cars(){
+    public ArrayList<Car> retrive_all_cars() {
         final ArrayList<Car> list_car = new ArrayList<Car>();
-
-        myRef.addValueEventListener(new ValueEventListener() {
+        myCarRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot carroSnap : dataSnapshot.getChildren()){
+                for (DataSnapshot carroSnap : dataSnapshot.getChildren()) {
                     Car car = carroSnap.getValue(Car.class);
                     car.key = carroSnap.getKey();
-                    System.out.println(car.key);
                     list_car.add(car);
                 }
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.w(TAG, "Failed to read value.", databaseError.toException());
@@ -59,10 +57,32 @@ public class FireControl {
         return list_car;
     }
 
-    public Car getCar(Car car) {
-
-
-        return  car;
+    public void writeNewClient(Client client){
+        String key = myClientRef.push().getKey();
+        Map<String, Object> clientValues = client.toMap();
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put(key, clientValues);
+        myClientRef.updateChildren(childUpdates);
     }
 
+    public ArrayList<Client> retrive_all_clients() {
+        final ArrayList<Client> list_client = new ArrayList<Client>();
+        myClientRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot carroSnap : dataSnapshot.getChildren()) {
+                    Client client = carroSnap.getValue(Client.class);
+                    client.key = carroSnap.getKey();
+                    list_client.add(client);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+
+            }
+        });
+
+        return list_client;
+    }
 }
